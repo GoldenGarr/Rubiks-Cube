b3ac1824829dfce245ca9fa41806ca3db623e56c
GoldenGarr <maxim20031128@gmail.com>
2022-06-04 08:52:28 +0300

    Setting the cross

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 #include "cube/Cube.h"
+#include "solver/Solver.h"
 
 int main() {
-    Cube().rotate("R");
-    Cube().print();
+    Cube cube = Cube().read_file("/Users/maksim/CLionProjects/RubiksCube/test_input.txt");
+//    Cube cube = Cube().generate_random();
+
+//    cube.print();
+
+    Solver solver;
+    solver.first_layer(cube);
+    cube.print();
+
+
+//    std::string rot;
+//    while (rot != "E") {
+//        std::cout << "Enter the direction:";
+//        std::cin >> rot;
+////        cube.rotate(rot);
+//        cube.rotate_cube(rot);
+//        cube.print();
+//    }
+
+//    cube.to_txt("ajsdjkadjs");
+
     return 0;
 }
